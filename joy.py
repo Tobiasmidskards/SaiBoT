244c7c356b8f8a0ae5c64721a930f7c91e231ef6
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-06-16 17:41:03 +0200

    Update joy.py

--- joy.py
+++ joy.py
@@ -39,7 +39,7 @@ axisUpDown = 1                          # Joystick axis to read for up / down po
 axisUpDownInverted = False 		# Set this to True if up and down appear to be swapped
 axisLeftRight = 0 			# Joystick axis to read for left / right position
 axisLeftRightInverted = False 		# Set this to True if left and right appear to be swapped
-interval = 0.3 				# Time between keyboard updates in seconds, smaller responds faster but uses more processor time
+interval = 0.1 				# Time between keyboard updates in seconds, smaller responds faster but uses more processor time
 
 
 # Setup pygame and key states
