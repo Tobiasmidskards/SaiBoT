dcd8e94b025ff3a14a6899eab255dc1bd11ec149
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-06-16 13:13:26 +0200

    Update joy.py

--- joy.py
+++ joy.py
@@ -188,7 +188,7 @@ try:
 			#GPIO.output(leftDriveCounter, rightStateCounter)
 			#GPIO.output(rightDriveCounter, rightStateCounter)
 			acceleration(7)
-			print('speedCount')
+			print(speedCount)
 			
 		# Wait for the interval period
 		time.sleep(interval)
