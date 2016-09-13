0673edc895567b1f6f346572d391afb18c4bed7d
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-09-13 22:18:33 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -88,11 +88,12 @@ def PygameHandler(events):
 			hadEvent = True
 			if event.key == pygame.K_ESCAPE:
 				moveQuit = True
+        
         elif event.type == pygame.KEYUP:
 		# A key has been released, see if it is one we want
 		hadEvent = True
-		if event.key == pygame.K_ESCAPE:
-			moveQuit = False
+		    if event.key == pygame.K_ESCAPE:
+			    moveQuit = False
 				
 		elif event.type == pygame.JOYBUTTONDOWN:
 			# a button has been pressed
