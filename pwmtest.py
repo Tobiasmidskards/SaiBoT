115351633e90f53f698c0ebe37be1ff5315880df
Tobias Midskard <Tobiasmidskard@Tobiass-MacBook-Pro.local>
2016-09-14 23:54:17 +0200

    hey

--- pwmtest.py
+++ pwmtest.py
@@ -5,12 +5,20 @@ GPIO.setmode(GPIO.BOARD)  # choose BCM or BOARD numbering schemes. I use BCM
 
 GPIO.setup(7, GPIO.OUT)# set GPIO 25 as output for white led
 GPIO.setup(11, GPIO.OUT)# set GPIO 24 as output for red led
+GPIO.setup(13, GPIO.OUT)# set GPIO 25 as output for white led
+GPIO.setup(15, GPIO.OUT)# set GPIO 24 as output for red led
 
-white = GPIO.PWM(7, 100)    # create object white for PWM on port 25 at 100 Hertz
-red = GPIO.PWM(11, 100)      # create object red for PWM on port 24 at 100 Hertz
+leftf = GPIO.PWM(7, 100)    # create object white for PWM on port 25 at 100 Hertz
+leftb = GPIO.PWM(11, 100)      # create object red for PWM on port 24 at 100 Hertz
 
-white.start(0)              # start white led on 0 percent duty cycle (off)
-red.start(100)              # red fully on (100%)
+rightf = GPIO.PWM(13, 100)    # create object white for PWM on port 25 at 100 Hertz
+rightb = GPIO.PWM(15, 100)      # create object red for PWM on port 24 at 100 Hertz
+
+leftf.start(0)              # start white led on 0 percent duty cycle (off)
+leftb.start(100)              # red fully on (100%)
+
+rightf.start(0)              # start white led on 0 percent duty cycle (off)
+rightb.start(100)              # red fully on (100%)
 
 # now the fun starts, we'll vary the duty cycle to
 # dim/brighten the leds, so one is bright while the other is dim
@@ -20,16 +28,20 @@ pause_time = 0.02           # you can change this to slow down/speed up
 try:
     while True:
         for i in range(0,101):      # 101 because it stops when it finishes 100
-            white.ChangeDutyCycle(i)
+            leftf.ChangeDutyCycle(i)
+            rightf.ChangeDutyCycle(i)
             #red.ChangeDutyCycle(100 - i)
             sleep(pause_time)
-        white.ChangeDutyCycle(0)
+        leftf.ChangeDutyCycle(0)
+        rightf.ChangeDutyCycle(0)
         sleep(1)
         for i in range(100,-1,-1):      # from 100 to zero in steps of -1
             #white.ChangeDutyCycle(i)
-            red.ChangeDutyCycle(100 - i)
+            leftb.ChangeDutyCycle(100 - i)
+            rightb.ChangeDutyCycle(100 - i)
             sleep(pause_time)
-        red.ChangeDutyCycle(0)
+        leftb.ChangeDutyCycle(0)
+        rightb.ChangeDutyCycle(0)
         sleep(1)
 
 except KeyboardInterrupt:
