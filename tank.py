eebb24f7f18e7f95385336adc7e6846b4b34462e
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-10-18 01:14:28 +0200

    a

--- tank.py
+++ tank.py
@@ -9,7 +9,7 @@ GPIO.setwarnings(False)
 # Settings for joystick
 axisUpDown = 1                          # Joystick axis to read for up / down position
 axisUpDownInverted = False 		# Set this to True if up and down appear to be swapped
-axisLeftRight = 0 			# Joystick axis to read for left / right position
+axisLeftRight = 3 			# Joystick axis to read for left / right position
 axisLeftRightInverted = False 		# Set this to True if left and right appear to be swapped
 interval = 0.1 				# Time between keyboard updates in seconds, smaller responds faster but uses more processor time
 
