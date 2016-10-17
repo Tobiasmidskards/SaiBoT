54c9ebce681517ad9b19f2cd6e17375a3640d8b0
Tobiasmidskards <fqn295@alumni.ku.dk>
2016-10-18 01:42:54 +0200

    a

--- tank.py
+++ tank.py
@@ -163,7 +163,7 @@ def PygameHandler(events):
              if left < 0:
                  up = left * (-1) * (100)
              elif left > 0:
-                 down = left * (-1)
+                 down = left * 100
 
              else:
                  print left
@@ -172,7 +172,7 @@ def PygameHandler(events):
                  upr = right * (-1) * (100)
 
              elif right > 0:
-                 downr = right * (-1)
+                 downr = right * 100
              else:
                  print right
 
